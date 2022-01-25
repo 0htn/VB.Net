a0d8ee442e11d13176f5ca41aacd46aefae06e5e
fourseasonsz <63898417+fourseasonsz@users.noreply.github.com>
2022-01-24 16:23:26 -0800

    Create Calculator.vb

---
+++ Calculator.vb
@@ -0,0 +1,41 @@
+	  Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+	    'Calculate the numeric expression
+	    On Error Resume Next
+	    Dim int1 As Integer 'From User
+	    Dim int2 As Integer 'From User
+	    Dim result As Double
+	    Dim operators As New List(Of String) From
+	      {"+", "-", "*", "/", "^"}
+
+	  int1 = txtIntOne.Text
+	  int2 = txtIntTwo.Text
+
+	    'Process the data
+	    For Each operato As String In operators
+	      operato = Operators_.Text
+	      If operato = "+" Then
+	        result = int1 + int2
+	      ElseIf operato = "-" Then
+	        result = int1 - int2
+	      ElseIf operato = "*" Then
+	        result = int1 * int2
+	      ElseIf operato = "/" Then
+	        result = int1 / int2
+	      Else
+	        result = int1 ^ int2
+	      End If
+	    Next
+
+	    'Output the result
+	    MsgBox(result)
+
+	  End Sub
+
+	  Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+	    'Clear inputs
+	    intOne.Clear()
+	    intTwo.Clear()
+	    Operators_.Clear()
+	  End Sub
+
+	End Class
