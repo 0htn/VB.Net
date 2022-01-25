8e56033b3a171e5087e66e7ad4b9310cc1e55c22
fourseasonsz <63898417+fourseasonsz@users.noreply.github.com>
2022-01-24 19:33:25 -0800

    Update Calculator.vb

--- Calculator.vb
+++ Calculator.vb
@@ -1,42 +1,43 @@
 Public Class Calculator
-	Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
-	    'Calculate the numeric expression
-	    On Error Resume Next
-	    Dim int1 As Integer 'From User
-	    Dim int2 As Integer 'From User
-	    Dim result As Double
-	    Dim operators As New List(Of String) From
-	      {"+", "-", "*", "/", "^"}
+  Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+		'Calculate the numeric expression
+		On Error Resume Next
+		Dim int1 As Integer 'From User
+		Dim int2 As Integer 'From User
+		Dim result As Double
+		Dim operators As New List(Of String) From
+				{"+", "-", "*", "/", "^"}
 
-	  int1 = intOne.Text
-	  int2 = intTwo.Text
+		int1 = txtIntOne.Text
+		int2 = txtIntTwo.Text
 
-	    'Process the data
-	    For Each operato As String In operators
-	      operato = Operators_.Text
-	      If operato = "+" Then
-	        result = int1 + int2
-	      ElseIf operato = "-" Then
-	        result = int1 - int2
-	      ElseIf operato = "*" Then
-	        result = int1 * int2
-	      ElseIf operato = "/" Then
-	        result = int1 / int2
-	      Else
-	        result = int1 ^ int2
-	      End If
-	    Next
+		'Process the data
+		For Each operato As String In operators
+			operato = Operators_.Text
+			If operato = "+" Then
+				result = int1 + int2
+			ElseIf operato = "-" Then
+				result = int1 - int2
+			ElseIf operato = "*" Then
+				result = int1 * int2
+			ElseIf operato = "/" Then
+				result = int1 / int2
+			ElseIf operato = "^" Then
+				result = int1 ^ int2
+			Else
+				result = 0
+			End If
+		Next
 
-	    'Output the result
-	    MsgBox(result)
+		'Output the result
+		MsgBox(result)
 
-	  End Sub
+	End Sub
 
-	  Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
-	    'Clear inputs
-	    intOne.Clear()
-	    intTwo.Clear()
-	    Operators_.Clear()
-	  End Sub
-
+	Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+		'Clear inputs
+		txtIntOne.Clear()
+		txtIntTwo.Clear()
+		Operators_.Clear()
+	End Sub
 End Class
